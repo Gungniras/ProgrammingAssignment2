eccdabdc10cc09c58fea6bd5f6b868f5b8ba218c
Gungniras <nikorapp1@gmail.com>
2020-04-22 19:53:55 +0200

    this one

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,8 @@
-Matrix inversion is usually a costly computation and there may be some
-benefit to caching the inverse of a matrix rather than computing it
-repeatedly (there are also alternatives to matrix inversion that we will
-            not discuss here). Your assignment is to write a pair of functions that
-cache the inverse of a matrix.
+## The 2 following functions work together to create and store the inverse of a matrix
+## If the inverse has been created before of this matrix, it is not recomputed but 
+## copied from the cache
 
-Write the following functions:
-    
-    1.  `makeCacheMatrix`: This function creates a special "matrix" object
-that can cache its inverse.
-2.  `cacheSolve`: This function computes the inverse of the special
-"matrix" returned by `makeCacheMatrix` above. If the inverse has
-already been calculated (and the matrix has not changed), then
-`cacheSolve` should retrieve the inverse from the cache.
-
-Computing the inverse of a square matrix can be done with the `solve`
-function in R. For example, if `X` is a square invertible matrix, then
-`solve(X)` returns its inverse.
-
-For this assignment, assume that the matrix supplied is always
-invertible.
-
-
-
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function stores the inverse of a matrix if computed before
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -42,9 +19,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
-## Return a matrix that is the inverse of 'x'
+## This function first checks if the inverse of the matrix has been computed before
+## if so it gets the inverse from the cache and prints it, if not it calculates and prints it
 
 cacheSolve <- function(x, ...) {
     m <- x$getinvertedmatrix()
@@ -59,9 +35,6 @@ cacheSolve <- function(x, ...) {
 }
 
 
-# test area
-
-testmatrix <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
 
 
 
